060a3d97e948cf5c2805631af347f784f8b1a630
MU-Software <musoftware@daum.net>
2024-05-25 19:18:41 +0900

    Fix AMI name for preventing aws_instance recreation

--- server/mudev-aws-1/infrastructures/modules/resources/ec2.tf
+++ server/mudev-aws-1/infrastructures/modules/resources/ec2.tf
@@ -10,12 +10,11 @@ resource "aws_network_interface" "mudev-ubuntu-network-interface" {
 }
 
 data "aws_ami" "ubuntu" {
-  most_recent = true
-  owners      = ["099720109477"] # Canonical
+  owners = ["099720109477"] # Canonical
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-20240423"]
   }
 
   filter {
