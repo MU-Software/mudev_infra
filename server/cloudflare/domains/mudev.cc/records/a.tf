93fe2870eaf8881348e8a98a722293091589f52d
MU-Software <musoftware@daum.net>
2025-01-06 10:07:16 +0900

    Add Cloudflare A records for AWS and Vultr origins

--- server/cloudflare/domains/mudev.cc/records/a.tf
+++ server/cloudflare/domains/mudev.cc/records/a.tf
@@ -1,9 +1,27 @@
+resource "cloudflare_record" "mudev_cc_record_a_aws_origin" {
+  name    = "aws-ec2-1-origin"
+  proxied = true
+  ttl     = 1
+  type    = "A"
+  content = var.mudev_aws_1_instance_ipv4
+  zone_id = var.cloudflare_zone_id
+}
+
 resource "cloudflare_record" "mudev_cc_record_a_api" {
   name    = "api"
   proxied = true
   ttl     = 1
   type    = "A"
   content = var.mudev_aws_1_instance_ipv4
+  zone_id = var.cloudflare_zone_id
+}
+
+resource "cloudflare_record" "mudev_cc_record_a_vultr_origin" {
+  name    = "vultr-origin"
+  proxied = true
+  ttl     = 1
+  type    = "A"
+  content = var.mudev_vultr_1_instance_ipv4
   zone_id = var.cloudflare_zone_id
 }
 
